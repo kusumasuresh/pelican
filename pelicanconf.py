8a80ebd837c74596d0c0a894ca1015a8c50f7fef
suresh <suresh@suresh.suresh.lintel>
2015-04-08 17:15:23 +0530

    initial comit

---
+++ pelicanconf.py
@@ -0,0 +1,10 @@
+   ('PELICAN_COMMENT_SYSTEM', True),
+        ('PELICAN_COMMENT_SYSTEM_DIR', '/test/comments'),
+        ('PELICAN_COMMENT_SYSTEM_IDENTICON_OUTPUT_PATH', 'images/identicon'),
+        ('PELICAN_COMMENT_SYSTEM_IDENTICON_DATA', ()),
+        ('PELICAN_COMMENT_SYSTEM_IDENTICON_SIZE', 72),
+        ('PELICAN_COMMENT_SYSTEM_AUTHORS', {}),
+        ('PELICAN_COMMENT_SYSTEM_FEED', os.path.join('feeds', 'comment.%s.atom.xml')),
+        ('PELICAN_COMMENT_SYSTEM_FEED_ALL', os.path.join('feeds', 'comments.all.atom.xml')),
+        ('COMMENT_URL', '#comment-{slug}')
+
